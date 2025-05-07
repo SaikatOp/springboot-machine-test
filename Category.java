9a2acef1f6df4f950e378ca9a99861a53f32e64b
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-07 20:22:09 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,3 +1,7 @@
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Category {
 
@@ -9,4 +13,31 @@ public class Category {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> products = new ArrayList<>();
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for products
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
