ade411a6ea2662b4983ee70ba79c72da9a808b17
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-07 20:06:59 +0530

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,12 @@
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+}
