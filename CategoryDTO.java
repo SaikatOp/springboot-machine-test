5940659eafcab9705589284f2c6c6d9ec61336de
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-07 20:20:47 +0530

    Update CategoryDTO.java

--- CategoryDTO.java
+++ CategoryDTO.java
@@ -1,4 +1,24 @@
 public class CategoryDTO {
     private Long id;
     private String name;
+
+    // Getter for id
+    public Long getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
