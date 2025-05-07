2a62bdbb93eb1594d7e5f5449484d5acb200a2b4
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-07 20:23:58 +0530

    Update ProductDTO.java

--- ProductDTO.java
+++ ProductDTO.java
@@ -3,4 +3,40 @@ public class ProductDTO {
     private String name;
     private double price;
     private CategoryDTO category;
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter and Setter for category
+    public CategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryDTO category) {
+        this.category = category;
+    }
 }
