4069ed81b463c7ae6ad221c8f14ad7722d1cfccc
SaikatOp <141167640+SaikatOp@users.noreply.github.com>
2025-05-07 20:23:08 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,5 @@
+import jakarta.persistence.*;
+
 @Entity
 public class Product {
 
@@ -11,4 +13,40 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter and Setter for category
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
